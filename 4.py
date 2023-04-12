e5489bab4bec7fb4573e97bb3beffd1aa358d134
San <yan_san_din@mail.ru>
2023-04-12 21:38:26 +0300

    added new file

---
+++ 4.py
@@ -0,0 +1,6 @@
+print('Введите общее количество журавликов: ')
+s = int(input())
+petya= s/6
+sergey= s/6
+katya = s-(sergey+petya)
+print(f"Количество журавликов каждого Петя: {petya}, Сергей: {sergey}, Катя: {katya}")
