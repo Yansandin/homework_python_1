bf4af1381cb6adf46c342416252cb9fea51dfdb8
San <yan_san_din@mail.ru>
2023-04-12 22:55:27 +0300

    added new file

---
+++ 8.py
@@ -0,0 +1,9 @@
+print('Введите размер шоколадки m x n')
+m = int(input())
+n= int(input())
+print('Введите размер дольки уоторую хотите отломить (k)')
+k = int(input())
+if k==m and k==n or k%n==0 or k%n==0:
+    print('yes')
+else:
+    print('no')
